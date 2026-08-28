2149901887490ac189ac5b1b9a1178035a1a58e9
Felipe Gomes Savino <felipegsavino@gmail.com>
2026-08-27 22:26:16 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
+import Image from "next/image"
+import { Input } from "./_components/ui/input"
+import Header from "./_components/ui/header"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
 
 const Home = () => {
-  return <Button>test</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p>Segunda-feira, 05 de agosto. </p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="mt-6 w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            width={1000}
+            height={300}
+
+            className="h-auto w-full rounded-xl"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
