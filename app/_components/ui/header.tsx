516946216bdc590aa2f95b603ecd4e39b9ba8e9b
Felipe Gomes Savino <felipegsavino@gmail.com>
2026-09-23 17:26:34 -0300

    feat: add sidebar

--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -1,19 +1,90 @@
 import Image from "next/image"
 import { Card, CardContent } from "./card"
 import { Button } from "./button"
-import { MenuIcon } from "lucide-react"
-
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./sheet"
+import { quickSearchOptions } from "@/app/_constants/search"
+import { Avatar, AvatarImage } from "./avatar"
+import Link from "next/link"
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="overflow-y-auto">
+            <SheetHeader>
+              <SheetTitle className="text-left">Menu</SheetTitle>
+            </SheetHeader>
+
+            <div className="flex items-center gap-3 border-b border-solid py-5">
+              <Avatar>
+                <AvatarImage src="https://preview.redd.it/the-xbox-360s-stock-avatar-pictures-v0-yta0xtiii3m21.jpg?width=640&crop=smart&auto=webp&s=5e798fff627a79715923806a0a07f74469902e21" />
+              </Avatar>
+
+              <div>
+                <p className="font-bold">Felipe Gomes Savino</p>
+                <p className="text-xs">felipe_fullstack@gmail.com</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              <SheetClose asChild>
+                <Button className="justify-start gap-2" variant="ghost" asChild>
+                  <Link href="/">
+                    <HomeIcon size={18} />
+                    Início
+                  </Link>
+                </Button>
+              </SheetClose>
+
+              <Button className="justify-start gap-2" variant="ghost">
+                <CalendarIcon size={18} />
+                Agendamentos
+              </Button>
+            </div>
+
+            <div className="py-5-5 flex flex-col gap-2 border-b border-solid">
+              {quickSearchOptions.map((option) => (
+                <Button
+                  key={option.title}
+                  className="justify-start gap-2"
+                  variant="ghost"
+                >
+                  <Image
+                    alt={option.title}
+                    src={option.imageUrl}
+                    height={18}
+                    width={18}
+                  />
+                  {option.title}
+                </Button>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2 py-5">
+              <Button variant="ghost" className="justify-start">
+                <LogOutIcon />
+                Sair da conta
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   )
 }
-
 export default Header
